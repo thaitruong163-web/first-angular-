251f9023547fd509b6be91c9a60baffd5d2218ab
Thai Truong <thaitruong163@gmail.com>
2025-12-31 17:15:00 +0700

    first_commit

---
+++ src/app/app.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  standalone: true,
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.html',
+  styleUrls: ['./app.scss']
+})
+export class App {}
